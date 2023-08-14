ccd1621610d5654bf3268f9fe7354e2c4c3dd8d3
ben-green-kerro <ben@kernowrobotics.co.uk>
2023-08-14 08:11:46 +0100

    converted to robot class

--- python_controller/pygameControl.py
+++ python_controller/pygameControl.py
@@ -4,64 +4,82 @@ import socket
 # from pygame.locals import *
 # import sys
 
-def send_command_to_arduino(command):
-    print(command)
-    try:
-        arduino_ip = "192.168.0.13"  # Replace this with the IP address of your Arduino
-        arduino_port = 333  # Use any available port number (greater than 1023)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((arduino_ip, arduino_port))
-        sock.sendall(command.encode())
-        sock.close()
+class Robot(object):
+    """
+    a class to do a job
+    """
 
-    except Exception as e:
-        print(f"Error sending command to Arduino: {e}")
+    def __init__(self):
+        """
+        Runs once when object is initialised
+        """
+        self.arduino_ip = "192.168.0.15"  # Replace this with the IP address of your Arduino
+        self.arduino_port = 333  # Use any available port number (greater than 1023)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.arduino_ip, self.arduino_port))
+        self.speeds = [90,90,90,90]
+        self.messageNumber = 0
+        self.lasttime = time.time()
 
-def set_servo_speeds(speed0, speed1, speed2, speed3):
-    command = f"S0{speed0};S1{speed1+1};S2{180-(speed2-3)};S3{speed3}\n"
-    send_command_to_arduino(command)
+    def keysToServos(self, keys):
+        self.speeds = [90,90,90,90]
+        if keys[0]==1:
+            self.speeds[1]+=90
+            self.speeds[2]+=90
+        if keys[2]==1:
+            self.speeds[1]-=90
+            self.speeds[2]-=90
+        if keys[1]==1:
+            self.speeds[1]-=90
+            self.speeds[2]+=90
+        if keys[3]==1:
+            self.speeds[1]+=90
+            self.speeds[2]-=90
+        for speed in self.speeds:
+            if speed > 180:
+                speed = 180
+            if speed < 0:
+                speed = 0
+        print(self.speeds)
+        self.set_servo_speeds(self.speeds[0], self.speeds[1], self.speeds[2], self.speeds[3])
 
-def keysToServos(keys):
-    speeds = [90,90,90,90]
-    if keys[0]==1:
-        speeds[1]+=90
-        speeds[2]+=90
-    if keys[2]==1:
-        speeds[1]-=90
-        speeds[2]-=90
-    if keys[1]==1:
-        speeds[1]-=90
-        speeds[2]+=90
-    if keys[3]==1:
-        speeds[1]+=90
-        speeds[2]-=90
-    for speed in speeds:
-        if speed > 180:
-            speed = 180
-        if speed < 0:
-            speed = 0
-    print(speeds)
-    set_servo_speeds(speeds[0], speeds[1], speeds[2], speeds[3])
+    def set_servo_speeds(self, speed0, speed1, speed2, speed3):
+        command = f"S0{speed0};S1{speed1+1};S2{180-(speed2-3)};S3{speed3}\n"
+        # command = f"S0{speed0};S1{speed1+1};S2{180-(speed2-3)};S3{speed3};{self.messageNumber}\n"
+        self.messageNumber+=1
+        # self.sock.close()
+        print(self.messageNumber)
+        print(1/(time.time()-self.lasttime))
+        self.lasttime = time.time()
+        self.send_command_to_arduino(command)
+
+    def send_command_to_arduino(self, command):
+        print(command)
+        try:
+            # self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # self.sock.connect((self.arduino_ip, self.arduino_port))
+            self.sock.sendall(command.encode())
+            # self.sock.close()
+
+        except Exception as e:
+            print(f"Error sending command to Arduino: {e}")
 
 
 def main():
+    robot = Robot()
     background_colour = (255,255,255)
     screen = pygame.display.set_mode((640, 480))
     screen.fill(background_colour)
     clock = pygame.time.Clock()
     pygame.display.set_caption("title")
-
     pygame.init()
     pygame.display.flip()
-    time.sleep(1)
+    # time.sleep(1)
     running = 1
     keys=[0,0,0,0]
     while running:
         # print("loop")
         for event in pygame.event.get():
-            # if (event.type == KEYUP) or (event.type == KEYDOWN):
-            #     print("key pressed")
-            #     time.sleep(0.1)
             if event.type == pygame.QUIT:
                 running = 0
             if event.type == pygame.KEYDOWN:
@@ -82,12 +100,12 @@ def main():
                     keys[2] = 0
                 if event.key == pygame.K_d:
                     keys[3] = 0
-        keysToServos(keys)
+        robot.keysToServos(keys)
         screen.fill(background_colour)
-        clock.tick(60)
         pygame.display.update()
         pygame.display.flip()
-        # pygame.time.wait(1)
+        time.sleep(0.3)
+
     pygame.quit()
 
 if __name__ == "__main__":
