756563df667a62cbf459f549518f13a44013380d
ben-green-kerro <ben@kernowrobotics.co.uk>
2024-02-08 23:30:09 +0000

    feat: keys and joys now detected and prioritised

--- python_controller/pygameControl.py
+++ python_controller/pygameControl.py
@@ -37,35 +37,66 @@ def main():
     pygame.init()
     text_print = TextPrint()
     keys=[0,0,0,0]
+    motors=[0,0,0,0,0,0]
     running = 1
     while running:
-        # print("loop")
         screen.fill((255,255,255))
         text_print.reset()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = 0
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    keys[0] = 1
-                if event.key == pygame.K_a:
-                    keys[1] = 1
-                if event.key == pygame.K_s:
-                    keys[2] = 1
-                if event.key == pygame.K_d:
-                    keys[3] = 1
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_w:
-                    keys[0] = 0
-                if event.key == pygame.K_a:
-                    keys[1] = 0
-                if event.key == pygame.K_s:
-                    keys[2] = 0
-                if event.key == pygame.K_d:
-                    keys[3] = 0
+            if event.type == pygame.JOYDEVICEADDED:
+                # This event will be generated when the program starts for every
+                # joystick, filling up the list without needing to create them manually.
+                joy = pygame.joystick.Joystick(event.device_index)
+                joysticks[joy.get_instance_id()] = joy
+                print(f"Joystick {joy.get_instance_id()} connencted")
+
+            if event.type == pygame.JOYDEVICEREMOVED:
+                del joysticks[event.instance_id]
+                print(f"Joystick {event.instance_id} disconnected")
+        # ---- GET KEY STATES ---- #
+        keyspressed = pygame.key.get_pressed()
+        keys[0] = keyspressed[pygame.K_w]*1
+        keys[1] = keyspressed[pygame.K_a]*1
+        keys[2] = keyspressed[pygame.K_s]*1
+        keys[3] = keyspressed[pygame.K_d]*1
+
+        # ---- GET JOYSTICK AXES ---- #
+        axes=[0,0,0,0,0,0,]
         joystick_count = pygame.joystick.get_count()
+        if joystick_count > 0:
+            print("howdy")
+            for joystick in joysticks.values():
+                axes = []
+                numaxes = joystick.get_numaxes()
+                print(numaxes)
+                for i in range(numaxes):
+                    axes.append(joystick.get_axis(i))
+                    print(joystick.get_axis(i))
+
+        # ---- SELECT CONTROL SUPPLY ---- #
+        selector = 0
+        if any(keys):
+            selector=1
+        elif any(axes):
+            selector=2
+        else:
+            selector=0
+        
+        # ---- CALCULATE MOTOR COMMANDS ---- #
+        if selector==0:
+            robot.set_servo_speeds(0,0,0,0,0,0)
+        elif selector==1:
+            robot.set_servo_speeds(0,0,0,0,0,0)
+        elif selector==2:
+            robot.set_servo_speeds(0,0,0,0,0,0)
+
+
         text_print.tprint(screen, f"Number of joysticks: {joystick_count}")
         text_print.tprint(screen, f"{keys}")
+        text_print.tprint(screen, f"{axes}")
+        text_print.tprint(screen, f"{selector}")
         # robot.keysToServos(keys)
         # screen.fill(background_colour)
         # pygame.display.update()
