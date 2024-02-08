a2af248157edfe1b70457524061bc221039152e5
ben-green-kerro <ben@kernowrobotics.co.uk>
2024-02-08 22:10:17 +0000

    Add controller interface to pygameControl

--- python_controller/pygameControl.py
+++ python_controller/pygameControl.py
@@ -2,21 +2,46 @@ from kernowRobotics import Guppy
 import pygame
 import time
 
+class TextPrint:
+    def __init__(self):
+        self.reset()
+        self.font = pygame.font.Font(None, 25)
+
+    def tprint(self, screen, text):
+        text_bitmap = self.font.render(text, True, (0, 0, 0))
+        screen.blit(text_bitmap, (self.x, self.y))
+        self.y += self.line_height
+
+    def reset(self):
+        self.x = 10
+        self.y = 10
+        self.line_height = 15
+
+    def indent(self):
+        self.x += 10
+
+    def unindent(self):
+        self.x -= 10
+
+def map_range(x, in_min, in_max, out_min, out_max):
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def main():
     robot = Guppy()
-    background_colour = (255,255,255)
     screen = pygame.display.set_mode((640, 480))
-    screen.fill(background_colour)
+    screen.fill((255,255,255))
     clock = pygame.time.Clock()
-    pygame.display.set_caption("title")
+    joysticks = {}
+    # joystick_count = pygame.joystick.get_count()
+    pygame.display.set_caption("Kernow Robotics Controller")
     pygame.init()
-    pygame.display.flip()
-    # time.sleep(1)
-    running = 1
+    text_print = TextPrint()
     keys=[0,0,0,0]
+    running = 1
     while running:
         # print("loop")
+        screen.fill((255,255,255))
+        text_print.reset()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = 0
@@ -38,11 +63,14 @@ def main():
                     keys[2] = 0
                 if event.key == pygame.K_d:
                     keys[3] = 0
-        robot.keysToServos(keys)
+        joystick_count = pygame.joystick.get_count()
+        text_print.tprint(screen, f"Number of joysticks: {joystick_count}")
+        text_print.tprint(screen, f"{keys}")
+        # robot.keysToServos(keys)
         # screen.fill(background_colour)
         # pygame.display.update()
-        # pygame.display.flip()
-        time.sleep(0.01)
+        clock.tick(60)
+        pygame.display.flip()
 
     pygame.quit()
 
--- python_controller/pygameControl2.py
+++ python_controller/pygameControl2.py
@@ -5,67 +5,6 @@ import socket
 def map_range(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-class Robot(object):
-    """
-    a class to do a job
-    """
-
-    def __init__(self):
-        """
-        Runs once when object is initialised
-        """
-        self.arduino_ip = "192.168.0.105"  # Replace this with the IP address of your Arduino
-        self.arduino_port = 333  # Use any available port number (greater than 1023)
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # self.sock.connect((self.arduino_ip, self.arduino_port))
-        self.speeds = [90,90,90,90]
-        self.messageNumber = 0
-        self.lasttime = time.time()
-
-    def keysToServos(self, keys):
-        powerstep = 127
-        self.speeds = [0,0,0,0]
-        if keys[2]==1:
-            self.speeds[0]+=powerstep
-            self.speeds[1]+=powerstep
-        if keys[0]==1:
-            self.speeds[0]-=powerstep
-            self.speeds[1]-=powerstep
-        if keys[3]==1:
-            self.speeds[0]+=powerstep
-            self.speeds[1]-=powerstep
-        if keys[1]==1:
-            self.speeds[0]-=powerstep
-            self.speeds[1]+=powerstep
-        # for speed in self.speeds:
-        #     if speed > 180:
-        #         speed = 180
-        #     if speed < 0:
-        #         speed = 0
-        print(self.speeds)
-        self.set_servo_speeds(self.speeds[0], self.speeds[1], self.speeds[2], self.speeds[3])
-
-    def set_servo_speeds(self, speed0, speed1, speed2, speed3):
-        command = f"S0{speed0};S1{speed1};S2{speed2};S3{speed3}\n"
-        # command = f"S0{speed0};S1{speed1+1};S2{180-(speed2-3)};S3{speed3};{self.messageNumber}\n"
-        self.messageNumber+=1
-        # self.sock.close()
-        print(self.messageNumber)
-        print(1/(time.time()-self.lasttime))
-        self.lasttime = time.time()
-        self.send_command_to_arduino(command)
-
-    def send_command_to_arduino(self, command):
-        print(command)
-        try:
-            # self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # self.sock.connect((self.arduino_ip, self.arduino_port))
-            self.sock.sendto(command.encode(), (self.arduino_ip, self.arduino_port))
-            # self.sock.close()
-
-        except Exception as e:
-            print(f"Error sending command to Arduino: {e}")
-
 def main():
     robot = Robot()
     pygame.init()
